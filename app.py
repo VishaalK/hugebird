58825bb4729f451b34a571b365050998e86f1d70
Vishaal Kalwani <vishaalkal@gmail.com>
2018-05-29 09:49:31 -0700

    Added very basic routes

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/flyers')
+def flyers():
+    # return 'Hi'
+    print(request.args)
+    # multi_dict = request.args
+    # for key in multi_dict:
+    #     print multi_dict.get(key)
+    #     print multi_dict.getlist(key)
+    start = request.args['start']
+    end = request.args['end']
+    location_id = request.args['location_id']
+    return "Start: %s, End: %s, location_id: %s" % (start, end, location_id)
+
+
+@app.route('/locations', defaults={'location_id': None})
+@app.route('/locations/<location_id>')
+def locations(location_id):
+    if not location_id:
+        return 'all'
+
+    return "location_id: %s" % (location_id)
